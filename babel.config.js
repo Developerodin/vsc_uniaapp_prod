142f0ee69629b9262d0dd5403132f11182823096
akshaypareek01 <akshay96102@gmail.com>
2025-09-09 18:26:02 +0530

    working

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,8 @@ export default function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [],
+    plugins: [
+      'react-native-reanimated/plugin',
+    ],
   };
 }
